70a3c2fd9439a8cf234a18ee57625e7e1bdbc78c
Cturner-24 <jct509@gmail.com>
2024-10-16 15:31:02 -0400

    Added some basic functions that we can build off of later.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/beeba/CLionProjects/untitled
+# Install script for directory: C:/Users/jct50/CLionProjects/SE-300-Firewall-Project-Fall-2024
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "D:/CLion 2024.2.2/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2024.2.2/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/beeba/CLionProjects/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/jct50/CLionProjects/SE-300-Firewall-Project-Fall-2024/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -3,6 +3,42 @@ void sussy() {
     printf("sussy amogus");
 }
 
+void ReadPacket() {
+
+}
+
+void DataHandler() {
+
+}
+
+void Firewall() {
+
+}
+
+void Website_Blacklist() {
+
+}
+
+void Hash() {
+
+}
+
+void FreeWillie() { //Will free the register from the packets data after we determine we do not want it
+
+}
+
+void SendPort() {
+
+}
+
+void ReceivePort() {
+
+}
+
+void IP_to_Domain() {//converts ip address to a domain name
+
+}
+
 
 int main(void) {
     printf("Hello, World!\n");
